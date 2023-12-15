1e8efd535f985e4b9261c624ca1e9476fbed2f6e
Alexander <woolfee52@gmail.com>
2023-12-15 16:50:38 +0300

    add forum db model and made migrations

--- SocialMediaProject/temporary/models.py
+++ SocialMediaProject/temporary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Forum(models.Model):
+    # class Status(models.IntegerChoices):
+    #     DRAFT = 0, 'Черновик'
+    #     PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255, verbose_name='Имя статьи')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(blank=True, default=None,
+                              null=True, verbose_name='Фотография')
+    time_create = models.DateTimeField(
+        auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(
+        auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True)
