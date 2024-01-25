a7306bcf9d528b58318af0d9f07908e083319c33
Alexander <woolfee52@gmail.com>
2024-01-25 07:33:07 +0300

    add user profile

--- SocialMediaProject/users/forms.py
+++ SocialMediaProject/users/forms.py
@@ -4,12 +4,12 @@ from django.contrib.auth import get_user_model
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Логин')
+    username = forms.CharField(label='Логин', widget=forms.TextInput)
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин')
+    username = forms.CharField(label='Логин', widget=forms.TextInput)
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(
         label='Повтор пароля', widget=forms.PasswordInput)
@@ -24,3 +24,15 @@ class RegisterUserForm(UserCreationForm):
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError('Такой E-mail уже существует!')
         return email
+
+
+class ProfileUserForm(forms.ModelForm):
+    username = forms.CharField(
+        disabled=True, label='Логин', widget=forms.TextInput)
+    email = forms.CharField(
+        disabled=True, label='E-mail', widget=forms.TextInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name',
+                  'last_name']
--- SocialMediaProject/users/views.py
+++ SocialMediaProject/users/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
+from typing import Any
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
-from .forms import LoginUserForm, RegisterUserForm
-from django.views.generic import CreateView
+from .forms import LoginUserForm, ProfileUserForm, RegisterUserForm
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
 
 class LoginUser(LoginView):
@@ -18,3 +21,16 @@ class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': 'Профиль пользователя'}
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('users:profile')
+
+    def get_object(self, queryset: QuerySet[Any] | None = ...) -> Model:
+        return self.request.user
