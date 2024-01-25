632d17bef631a4c27b11801116d99a6c6e36d8f7
Alexander <woolfee52@gmail.com>
2024-01-25 23:29:56 +0300

    add password reset

--- SocialMediaProject/SocialMediaProject/settings.py
+++ SocialMediaProject/SocialMediaProject/settings.py
@@ -133,3 +133,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend",
                            'users.authentication.EmailAuthBackend',]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- SocialMediaProject/users/urls.py
+++ SocialMediaProject/users/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from . import views
 
 app_name = 'users'
@@ -13,4 +13,13 @@ urlpatterns = [
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+    path('password-reset/',
+         PasswordResetView.as_view(template_name='users/password_reset_form.html', email_template_name='users/password_reset_email.html', success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
+    path('password-reset/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
