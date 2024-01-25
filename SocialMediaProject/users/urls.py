a5f8e9082c609bd4fd7b293b4fdef08a5afae326
Alexander <woolfee52@gmail.com>
2024-01-25 07:42:26 +0300

    add password change for profile

--- SocialMediaProject/users/urls.py
+++ SocialMediaProject/users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from . import views
 
 app_name = 'users'
+
 
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
